1cb6d65fa5a828cb9002759c8e1b920eda0ced4a
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-27 11:19:29 -0800

    Add homepage and started /begin route

--- app.py
+++ app.py
@@ -3,7 +3,22 @@ from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config["SECRET_KEY"] = "never-tell!"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get("/")
+def homepage():
+
+    return render_template(
+        "survey_start.html", title=survey.title, instructions=survey.instructions
+    )
+
+
+@app.get("/begin")
+def start_survey():
+    pass
